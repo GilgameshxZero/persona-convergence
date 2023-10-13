f6d9fa7de8f653f13f4067205a8833a42d7627aa
GILGAMESH <~@gilgamesh.cc>
2023-10-12 21:48:23 -0700

    gpt2 model

---
+++ readme.md
@@ -0,0 +1,6 @@
+```bash
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install --upgrade -r requirements/pre.txt
+pip install --upgrade -r requirements/gpu.txt
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+# Shared library for utulek Python code.
+import setuptools
+
+setuptools.setup(
+    name="persona_convergence",
+    packages=["persona_convergence"],
+
+    install_requires=["numpy", "matplotlib", "scikit-learn"]
+)
