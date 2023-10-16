cd240d0b7a29d216ada0cf84ce7aabd6d6e03280
GILGAMESH <~@gilgamesh.cc>
2023-10-15 20:30:08 -0400

    req

--- setup.py
+++
@@ -1,9 +0,0 @@
-# Shared library for utulek Python code.
-import setuptools
-
-setuptools.setup(
-    name="persona_convergence",
-    packages=["persona_convergence"],
-
-    install_requires=["numpy", "matplotlib", "scikit-learn"]
-)
